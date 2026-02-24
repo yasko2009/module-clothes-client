5e423f93154f82ad8396fd2f917512d6df0a7178
yasko2009 <kuzychenko2007@gmail.com>
2026-02-24 15:26:39 +0300

    first commit

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import {Input} from 'antd'
 const { Search } = Input;
@@ -5,7 +7,7 @@ const onSearch = (value, _e, info) => console.log(info?.source, value);
 const SDF = () => {
   return (
     <div>
-       <Search placeholder="input search text" onSearch={onSearch} enterButton />
+       <Search style={{width:500, marginLeft: 600, marginTop: 40}} placeholder="input search text" onSearch={onSearch} enterButton />
     </div>
   )
 }
