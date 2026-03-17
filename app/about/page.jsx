3bcea7cb4abf8bea4bab8c84c6940a2c44a75fa6
yasko2009 <kuzychenko2007@gmail.com>
2026-03-17 15:20:23 +0300

    first commit

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,13 +1,14 @@
 'use client'
-
+import style from '../about/Page.module.css'
 import React from 'react'
 import {Input} from 'antd'
 const { Search } = Input;
 const onSearch = (value, _e, info) => console.log(info?.source, value);
 const SDF = () => {
   return (
-    <div>
-       <Search style={{width:500, marginLeft: 600, marginTop: 40}} placeholder="input search text" onSearch={onSearch} enterButton />
+    <div className='search'>
+       <Search style={{width:400, marginLeft: 600, marginTop: 40, marginLeft: 1300}} placeholder="input search text" onSearch={onSearch} enterButton />
+
     </div>
   )
 }
